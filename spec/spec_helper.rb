bbd4c4029d721c1ed3d3ba7bd1c4912a3285f625
Roman Myrka <rmyrka@assurance.com>
2024-05-19 15:52:18 +0300

    Add coverage check

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
 SimpleCov.start 'rails'
+SimpleCov.minimum_coverage 90
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
